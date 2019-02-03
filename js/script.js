f98e0ea4a35e80ad83f5f4157446cdc57c0c134a
alexandra <le.onze.mai@gmail.com>
2019-02-03 14:07:50 +0300

    realised input value

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 var $buttonsDigitCollection = document.querySelectorAll('.js-button-digit');
+var $inputDisplay = document.querySelector('.js-display');
+
+function getValue($something) {
+    return $something.value;
+};
 
 $buttonsDigitCollection.forEach(function($element) {
-  $element.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log(event)
-  })
-})
+    $element.addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log(getValue($element));
+        $inputDisplay.value = getValue($element);
+    });
+});
+
+
+
+
+
+
