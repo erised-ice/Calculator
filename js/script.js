9393af2459a583cf834093cf99588713cfdc1d65
alexandra <le.onze.mai@gmail.com>
2019-02-11 19:34:58 +0300

    made several digits in display

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-var $buttonsDigitCollection = document.querySelectorAll('.js-button-digit');
-var $inputDisplay = document.querySelector('.js-display');
-
+const $buttonsDigitCollection = document.querySelectorAll('.js-button-digit');
 function getValue($something) {
     return $something.value;
 };
 
+const $inputDisplay = document.querySelector('.js-display');
+
 $buttonsDigitCollection.forEach(function($element) {
     $element.addEventListener('click', function(event) {
         event.preventDefault();
-        console.log(getValue($element));
-        $inputDisplay.value = getValue($element);
+        $inputDisplay.value += getValue($element);
     });
 });
 
 
+// следующие строки не относятся к необходимому коду:
+var $messege;
+$messege = 'Hello!';
 
-
-
-
+// console.log(typeof $buttonsDigitCollection);
+// console.log(typeof getValue);
+// console.log(typeof $inputDisplay);
+// console.log(typeof $messege);
+// console.log(typeof $inputDisplay.value);
