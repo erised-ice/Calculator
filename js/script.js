7a29fc55a50c6ba2d413262a4431d45f161ca2d9
alexandra <le.onze.mai@gmail.com>
2019-04-18 17:44:11 +0300

    late commit

--- js/script.js
+++ js/script.js
@@ -3,22 +3,13 @@ function getValue($something) {
     return $something.value;
 };
 
+var number1=null;
 const $inputDisplay = document.querySelector('.js-display');
 
 $buttonsDigitCollection.forEach(function($element) {
     $element.addEventListener('click', function(event) {
         event.preventDefault();
         $inputDisplay.value += getValue($element);
+        number1 = $inputDisplay.value;
     });
 });
-
-
-// следующие строки не относятся к необходимому коду:
-var $messege;
-$messege = 'Hello!';
-
-// console.log(typeof $buttonsDigitCollection);
-// console.log(typeof getValue);
-// console.log(typeof $inputDisplay);
-// console.log(typeof $messege);
-// console.log(typeof $inputDisplay.value);
