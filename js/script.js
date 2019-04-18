6150ea16e3cb680cc7b2940de3d267e1df862c65
alexandra <le.onze.mai@gmail.com>
2019-04-18 18:37:54 +0300

    added descriptions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const $buttonsDigitCollection = document.querySelectorAll('.js-button-digit');
+const $buttonsDigitCollection = document.querySelectorAll('.js-button-digit'); // собирает все цифровые кнопки по классу, тип: Объект
+const $buttonsOperationCollection = document.querySelectorAll('js-button-operations'); // собирает все операционные кнопки по классу, тип: Объект
+
 function getValue($something) {
     return $something.value;
-};
+}; // Возвращает значение value у аргумента
 
-var number1=null;
-const $inputDisplay = document.querySelector('.js-display');
+const $inputDisplay = document.querySelector('.js-display'); // Создает константу в виде элемента с классом в скобках (целый кусок html) (Инпут дисплей)
 
-$buttonsDigitCollection.forEach(function($element) {
-    $element.addEventListener('click', function(event) {
-        event.preventDefault();
-        $inputDisplay.value += getValue($element);
-        number1 = $inputDisplay.value;
+$buttonsDigitCollection.forEach(function($element) { // проходит циклом по цифровым кнопкам ($buttonsDigitCollection)
+    $element.addEventListener('click', function(event) { // навешивает событие 'click' на каждый элемент, по которому прошлись циклом, добавляет функцию в ответ на совершенное событие 'click'. Т.е. функция сработает, если по кнопке кликнут.
+        event.preventDefault(); // сброс действий по умолчанию
+        $inputDisplay.value += getValue($element); // Увеличивает в цикле value у $inputDisplay на value от $element (той кнопки, по которой кликнули)
     });
 });
