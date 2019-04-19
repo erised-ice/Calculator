e993e8f32506ec82c91d6c6571eda229b93404f6
alexandra <le.onze.mai@gmail.com>
2019-04-19 15:18:32 +0300

    added js-code from late version

--- js/script.js
+++ js/script.js
@@ -1,15 +1,56 @@
-const $buttonsDigitCollection = document.querySelectorAll('.js-button-digit'); // собирает все цифровые кнопки по классу, тип: Объект
-const $buttonsOperationCollection = document.querySelectorAll('js-button-operations'); // собирает все операционные кнопки по классу, тип: Объект
+function getInputValue(id) {
+  return document.getElementById(id).value;
+}
 
-function getValue($something) {
-    return $something.value;
-}; // Возвращает значение value у аргумента
+function stringToNumber(string) {
+  return +string;
+}
 
-const $inputDisplay = document.querySelector('.js-display'); // Создает константу в виде элемента с классом в скобках (целый кусок html) (Инпут дисплей)
+const $outputDisplay = document.querySelector('.js-output');
+const $buttonsOperationsCollection = document.querySelectorAll('.js-button-operations');
 
-$buttonsDigitCollection.forEach(function($element) { // проходит циклом по цифровым кнопкам ($buttonsDigitCollection)
+function output(result) {
+  $outputDisplay.value = result;
+}
+
+function calculate(event) {
+  const button = event.target;
+  const buttonValue = button.dataset.operation;
+  const inputValueOne = getInputValue('first-num');
+  const inputValueTwo = getInputValue('second-num');
+  const numberOne = stringToNumber(inputValueOne);
+  const numberTwo = stringToNumber(inputValueTwo);
+
+  let result = null;
+
+  switch (buttonValue) {
+    case 'addition':
+      result = numberOne + numberTwo;
+      break;
+    case 'subtraction':
+      result = numberOne - numberTwo;
+      break;
+    case 'multiply':
+      result = numberOne * numberTwo;
+      break;
+    case 'division':
+      result = numberOne / numberTwo;
+      break;
+    case 'reset':
+      document.getElementById('first-num').value = '';
+      document.getElementById('second-num').value = '';
+      result = '';
+  }
+
+  output(result);
+}
+
+$buttonsOperationsCollection.forEach(function (button) {
+  button.addEventListener('click',calculate);
+});
+/*$buttonsDigitCollection.forEach(function($element) { // проходит циклом по цифровым кнопкам ($buttonsDigitCollection)
     $element.addEventListener('click', function(event) { // навешивает событие 'click' на каждый элемент, по которому прошлись циклом, добавляет функцию в ответ на совершенное событие 'click'. Т.е. функция сработает, если по кнопке кликнут.
         event.preventDefault(); // сброс действий по умолчанию
-        $inputDisplay.value += getValue($element); // Увеличивает в цикле value у $inputDisplay на value от $element (той кнопки, по которой кликнули)
+        $inputDisplayOne.value += getValue($element); // Увеличивает в цикле value у $inputDisplay на value от $element (той кнопки, по которой кликнули)
     });
-});
+});*/
